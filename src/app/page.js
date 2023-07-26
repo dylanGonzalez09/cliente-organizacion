e3d73bff0bee2311f040f902276f661b6ce59c8a
Dylan Gonzalez <dyg09032003@gmail.com>
2023-07-25 19:56:00 -0500

    primer commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,94 @@
-import Image from 'next/image'
+'use client';
+import { useEffect, useState } from 'react';
+
+const URL = 'http://192.168.1.224:4000/api/file';
+
+const getFile = async (setFileContent) => {
+  try {
+    const res = await fetch(URL);
+    const data = await res.json();
+    setFileContent(data);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const handleSubmit = async (e, contentForm, setFileContent, setContentForm) => {
+  e.preventDefault();
+
+  try {
+    if (contentForm === '') return;
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ newInfo: contentForm }),
+    };
+
+    const res = await fetch(URL, options);
+    const data = await res.json();
+    setFileContent(data);
+    setContentForm('');
+  } catch (error) {
+    throw error;
+  }
+};
 
 export default function Home() {
+  const [fileContent, setFileContent] = useState({ msg: '', content: '' });
+  const [contentForm, setContentForm] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      await getFile(setFileContent);
+    })();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
+    <main className="flex flex-col items-center justify-between p-24 gap-40">
+      <h1 className="text-4xl font-bold">Manager de Archivo</h1>
+      <div className="w-6/12 flex flex-col items-center gap-5">
+        <h2 className="text-2xl font-semibold">Contenido del archivo</h2>
+        <p className="flex flex-col items-center text-2xl">
+          Mensaje informativo del servidor:{' '}
+          <span className="text-xl text-yellow-200">{fileContent.msg}</span>
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        {fileContent.content && (
+          <p className="flex flex-col items-center text-2xl">
+            Contenido del archivo localizado en el servidor:{' '}
+            <span className="text-yellow-200 text-xl">
+              {fileContent.content}
+            </span>
+          </p>
+        )}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="w-6/12">
+        <form
+          onSubmit={(e) =>
+            handleSubmit(e, contentForm, setFileContent, setContentForm)
+          }
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <div className="flex flex-col w-full items-center">
+            <label className="text-2xl font-bold mb-2">
+              Agrega Contenido al Archivo
+            </label>
+            <textarea
+              placeholder="Ingresa el contenido de tu archivo aqui..."
+              className="rounded-md p-1 w-full text-gray-600 placeholder-black outline-none mb-5 text-xl"
+              onChange={(e) => setContentForm(e.target.value)}
+              value={contentForm}
+            ></textarea>
+            <button
+              type="submit"
+              className={`bg-blue-900 rounded-lg p-2 font-bold text-xl hover:bg-blue-600 transition-colors duration-300`}
+            >
+              Enviar al Servidor
+            </button>
+          </div>
+        </form>
       </div>
     </main>
-  )
+  );
 }
